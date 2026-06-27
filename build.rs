51ba40b410c39882fcadb0b95761c5509d4c3444
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-27 11:45:42 +0200

    Version: use +dev suffix instead of -dirty

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     let output = Command::new("git")
-        .args(["describe", "--always", "--dirty"])
+        .args(["describe", "--always", "--dirty=+dev"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
